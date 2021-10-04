1506a1dd7c834c1d875ad2df5faca61234942b56
Jorge Yanar <jorgeyanar@gmail.com>
2021-10-04 16:49:17 -0400

    small window, check occupation before placing egg

--- main.lua
+++ main.lua
@@ -17,12 +17,15 @@
 -- [x] Tail gets longer when you eat an egg
 -- [x] Walls kill you
 -- [x] Tail collisions kill you
--- [ ] Inputting two keys in quick succession should result in that execution
---  [ ] Construct 2-length key buffer
+-- [x] Inputting two keys in quick succession should result in that execution
+--  [x] Construct 2-length key buffer, remove the nextdir functionality
+-- [x] Pause menu
+-- [x] Egg does not spawn on squares snake is currently occupying
+-- [ ] High score menu
 -- [ ] Check whether killed on next turn, not after the fact
+-- [ ] Smooth movement, but on a grid?
+require 'utils'
 
--- It's probably easier to move towards a grid system where movement is dictated
--- by timers instead of this dt * speed calculation.
 
 function love.load()
     grid = {
@@ -34,65 +37,62 @@ function love.load()
         y = 10,
         alive = true,
         currdir = 'down',
-        nextdir = 'down',
         speed = 0.1,
         tail = {[1] = {x = 10, y = 9},
                 [2] = {x = 10, y = 8},
                 [3] = {x = 10, y = 7}}
     }
+    dirbuffer = {[1] = '', [2] = ''}
     egg = {
         x = love.math.random(0, gw / 10),
         y = love.math.random(0, gh / 10)
     }
-    buffer = {[1] = '', [2] = ''}
     time = love.timer.getTime()
 end
 
 function love.update(dt)
-    -- If alive, then update
-    if snake.alive then
+    if ispaused then
+
+    elseif snake.alive then
         -- Update snake position, if requisite time has passed
         if (love.timer.getTime() - time) > snake.speed then
             time = love.timer.getTime()
             -- Check whether user has provided input
-            if snake.currdir ~= snake.nextdir then
-                movesnake(snake, snake.nextdir)
-                snake.currdir = snake.nextdir
-            else
+            if dirbuffer[1] == '' then
                 movesnake(snake, snake.currdir)
+            else
+                movesnake(snake, dirbuffer[1])
+                snake.currdir = dirbuffer[1]
+                -- Update dirbuffer
+                dirbuffer[1] = dirbuffer[2]
+                dirbuffer[2] = ''
             end
+
             -- Hit wall, die
             if snake.x > grid.width - 1  then snake.alive = false end
             if snake.x < 0               then snake.alive = false end
             if snake.y > grid.height - 1 then snake.alive = false end
             if snake.y < 0               then snake.alive = false end
             -- Hit self, die
-            -- Check whether head segment is hitting any tail segments. If so,
-            -- die
             for _, seg in ipairs(snake.tail) do
-                if snake.x == seg.x and snake.y == seg.y then
-                    snake.alive = false
-                end
+                if is_same_location(snake, seg) then snake.alive = false end
             end
 
-            -- -- Wrap around
-            -- if snake.x > grid.width - 1  then snake.x = 0           end
-            -- if snake.x < 0               then snake.x = grid.width  end
-            -- if snake.y > grid.height - 1 then snake.y = 0           end
-            -- if snake.y < 0               then snake.y = grid.height end
             -- Egg consumed
-            if (egg.x == snake.x) and (egg.y == snake.y) then
-                -- Update egg location
+            if is_same_location(snake, egg) then
+                -- Update egg location -- just don't generate it on an occupied square
                 egg.x, egg.y = love.math.random(1, grid.width - 1), love.math.random(1, grid.height - 1)
+                while is_occupied(snake, egg) do
+                    egg.x, egg.y = love.math.random(1, grid.width - 1), love.math.random(1, grid.height - 1)
+                end
                 -- Snake moves faster now
-                snake.speed = snake.speed - 0.01
+                snake.speed = snake.speed - 0.001
                 -- Add another tail segment
                 local lastsegment = snake.tail[#snake.tail]
                 snake.tail[#snake.tail + 1] = {x = lastsegment.x, y = lastsegment.y}
             end
         end
     else
-        -- local x = io.read()
     end
 end
 
@@ -100,21 +100,32 @@ end
 function love.draw()
     -- Let's make the grid have points every 10 pixels. To do this,
     -- we round down to the nearest factor of 10
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.rectangle('fill', snake.x * 10, snake.y * 10, 10, 10)
-    for isegment, segment in ipairs(snake.tail) do
-        love.graphics.rectangle('fill', segment.x * 10, segment.y * 10, 10, 10)
+    if ispaused then
+        love.graphics.printf('Press esc or p to continue', 0, 100, 400, 'center')
+    else
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.rectangle('fill', snake.x * 10, snake.y * 10, 10, 10)
+        for isegment, segment in ipairs(snake.tail) do
+            love.graphics.rectangle('fill', segment.x * 10, segment.y * 10, 10, 10)
+        end
+        love.graphics.setColor(1, 0, 0)
+        love.graphics.rectangle('fill', egg.x * 10, egg.y * 10, 10, 10)
     end
-    love.graphics.setColor(1, 0, 0)
-    love.graphics.rectangle('fill', egg.x * 10, egg.y * 10, 10, 10)
 end
 
 
 function love.keypressed(key)
-    if     key == 'up'    and snake.currdir ~= 'down'  then snake.nextdir = 'up'
-    elseif key == 'down'  and snake.currdir ~= 'up'    then snake.nextdir = 'down'
-    elseif key == 'left'  and snake.currdir ~= 'right' then snake.nextdir = 'left'
-    elseif key == 'right' and snake.currdir ~= 'left'  then snake.nextdir = 'right'
+    if key == 'up' or key == 'down' or key == 'left' or key == 'right' then
+        if     dirbuffer[1] == '' and isvaliddir(snake.currdir, key) then dirbuffer[1] = key
+        elseif dirbuffer[2] == '' and isvaliddir(dirbuffer[1], key)  then dirbuffer[2] = key
+        end
+    elseif key == 'escape' or key == 'p' then
+        -- Pause menu
+        if ispaused then
+            ispaused = false
+        else
+            ispaused = true
+        end
     end
 end
 
@@ -146,6 +157,28 @@ function movesnake(snake, dir)
         snake.x = snake.x + 1
     end
 end
+
+
+function is_same_location(a, b)
+    if a.x == b.x and a.y == b.y then
+        return true
+    else
+        return false
+    end
+end
+
+-- Check if the obj is located on a gridpoint occupied
+-- by the snake or any of its segments
+function is_occupied(snake, obj)
+    if is_same_location(snake, obj) then return true end
+    for _, segment in ipairs(snake.tail) do
+        if is_same_location(segment, obj) then
+            return true
+        end
+    end
+    return false
+end
+
 
 
 
